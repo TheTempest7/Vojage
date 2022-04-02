aa7c141d2d4a4c68dd661c082abe50554ea27243
TheTempest7 <galaxy.yegor.uaz.s4@gmail.com>
2022-04-02 20:05:46 +0300

    juniorIHoleLastCommit

---
+++ js/alert.js
@@ -0,0 +1,12 @@
+
+    let winWidth=window.innerWidth;
+window.addEventListener('resize',function(){
+    let winWidth=window.innerWidth;
+    if (winWidth<1080){
+        alert('Please leave this cite because it`s not adaptived for your device')
+    }
+})
+
+if (winWidth<1080){
+    alert('Please leave this cite because it`s not adaptived for your device')
+}
